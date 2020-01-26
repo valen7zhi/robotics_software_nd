8405cb1bcbd8cf0e99e4395d89c1ea9854834c29
valen7zhi <val103@outlook.com>
2020-01-25 16:20:46 -0800

    bugs fixed

--- project2_chase_ball/ball_chaser/src/drive_bot.cpp
+++ project2_chase_ball/ball_chaser/src/drive_bot.cpp
@@ -1,44 +1,63 @@
-#include "ros/ros.h"
-#include "geometry_msgs/Twist.h"
-#include <std_msgs/Float64.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
 #include "ball_chaser/DriveToTarget.h"
 
-// ROS::Publisher motor commands;
-ros::Publisher motor_command_publisher;
 
-bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
-    ball_chaser::DriveToTarget::Response& res)
-{  
-    // Create a motor_command object of type geometry_msgs::Twist
-    geometry_msgs::Twist motor_command;
-    // Set wheel velocities
-    motor_command.linear.x = req.linear_x;
-    motor_command.angular.z = req.angular_z;
-    // Publish angles to drive the robot
-    motor_command_publisher.publish(motor_command);
+/* A global ros::Publisher to publish motor commands */
+ros::Publisher motor_cmd_pub;
 
-    res.msg_feedback = "Velocities set - linear_x: " + std::to_string(req.linear_x) + ", angular_z: " + std::to_string(req.angular_z);
-    ROS_INFO_STREAM(res.msg_feedback);
-    return true;
+
+/* Callback function */
+// Objectives:
+// 1) To publish requested linear_x and angular_z
+// 2) To feedback the message after publishing the requested velocities
+bool drive_request_callback(ball_chaser::DriveToTarget::Request& req,
+                            ball_chaser::DriveToTarget::Response& res)
+{
+  ROS_INFO("DriveToTargetRequest received - linear_x: %1.2f, angular_z: %1.2f", (float)req.linear_x, (float)req.angular_z);
+
+  // Publish command velocity to "/cmd_vel"
+  geometry_msgs::Twist cmd_vel_;
+
+  cmd_vel_.linear.x = req.linear_x; // both of them are float64
+  cmd_vel_.angular.z = req.angular_z; // both of them are float64
+  
+  motor_cmd_pub.publish(cmd_vel_);
+
+  // Return a response message
+  res.msg_feedback = "Velocity cmd sent: linear_x = " + std::to_string(cmd_vel_.linear.x) + ", angular_z = " + std::to_string(cmd_vel_.angular.z);
+
+  ROS_INFO_STREAM(res.msg_feedback);
+  
+
+  return true;
+
 }
 
 int main(int argc, char** argv)
 {
-    // Initialize a ROS node
-    ros::init(argc, argv, "drive_bot");
+  // Initialize a ROS node
+  ros::init(argc, argv, "drive_bot");
 
-    // Create a ROS NodeHandle object
-    ros::NodeHandle n;
+  // Create a ROS NodeHandle object
+  ros::NodeHandle nh;
 
-    // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
-    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+  // Inform ROS master that we will be publishing a message,
+  // which is in a type of geometry_msgs::Twist,
+  // on the robot actuation topic, with a publish queue size 10
+  motor_cmd_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
-    // Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
-    ros::ServiceServer service = n.advertiseService("/ball_chaser/command_robot", handle_drive_request);
-    ROS_INFO("Robot is ready to receive velocity commands ...");
+  // Define a drive /ball_chaser/command_robot service with the callback function
+  ros::ServiceServer service = nh.advertiseService("/ball_chaser/command_robot", drive_request_callback);
+  ROS_INFO("Ready to send command velocities");
 
-    // Handle ROS communication events
-    ros::spin();
 
-    return 0;
+  // Handle ROS communication events
+  ros::spin();
+
+
+  return 0;
+
 }
+
+
